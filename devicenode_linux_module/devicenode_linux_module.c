b0150316bb5a312e9b8b171cfc87b1e3eeddafc7
374565452 <kong42315@163.com>
2017-02-21 17:26:31 +0800

    @2017-2-21 17:26增加生成设备节点测试代码

--- devicenode_linux_module/devicenode_linux_module.c
+++ devicenode_linux_module/devicenode_linux_module.c
@@ -1,4 +1,4 @@
-//注册设备驱动
+//生成设备节点信息
 /**
  * 包含初始化宏定义的头文件，代码中的module_init与module_exit在此文件中
  */
@@ -11,22 +11,70 @@
  * 驱动注册头文件，包括驱动注册结构体、注册与卸载函数
  */
 #include <linux/platform_device.h>
+/**
+ * 杂项设备结构体描述头文件，包括设备的注册、设备的卸载
+ */
+#include <linux/miscdevice.h>
+/**
+ * 注册设备文件节点的结构体头文件
+ */
+#include <linux/fs.h>
 
 //驱动注册名称，此名称一定要与注册的设备名称对应
 #define DRIVER_NAME "hello_ctl" 
+//生成的设备节点名称，此名称显示在/dev目录下，应用程序使用此文件名称来实现应用
+#define DEVICE_NAME "hello_ctl123"
 
 MODULE_LICENSE("Dual BSD/GPL"); //声明是开源的，没有内核版本限制
 MODULE_AUTHOR("KQZ"); //声明作者
 
+/**********start generate devicenode*********/
+static int hello_open  (struct inode * node, struct file * f)
+{
+	printk(KERN_EMERG"\t generate the device node in hello open function----------\n");
+	return 0;
+}
+static int hello_release (struct inode * node , struct file * f)
+{
+	printk(KERN_EMERG"\t generate the device node in hello relese function ------\n");
+	return 0;
+}
+static long hello_ioctl(struct file * f, unsigned int cmd , unsigned long arg)
+{
+	printk(KERN_EMERG"\t generate device node the cmd is %d ,arg is %d",cmd,arg);
+	return 0;
+}
+static struct file_operations hello_ops={
+	.owner=THIS_MODULE,
+	.open=hello_open,
+	.release=hello_release,
+	.unlocked_ioctl=hello_ioctl,
+};
 
+static struct miscdevice hello_dev={
+	.minor = MISC_DYNAMIC_MINOR, //自动生成设备节点次设备号
+	.name = DEVICE_NAME,
+	.fops = &hello_ops,
+};
+
+
+/**********end generate devicenode*********/
 static int hello_probe(struct platform_device * pdv)
 {
+	int miscdevice_state;
 	printk(KERN_EMERG "\tthe hello driver probe init -----\n");
+	//进行杂项设备的注册
+	miscdevice_state=misc_register(&hello_dev);
+	printk(KERN_EMERG"\t the register miscdevice state is %d \n",miscdevice_state);
 	return 0;
 }
 
 static int hello_remove(struct platform_device * pdv)
 {
+	int miscdevice_state;
+	//进行杂项设备的卸载
+	miscdevice_state=misc_deregister(&hello_dev);
+	printk(KERN_EMERG"\t the deregister miscdevice state is %d \n",miscdevice_state);
 	return 0;
 }
 
